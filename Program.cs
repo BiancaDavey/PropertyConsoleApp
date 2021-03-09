e43af271561f51c2e4be7b2cf7f9b88c13ebf54b
BiancaDavey <biancamdavey@icloud.com>
2021-03-09 13:43:15 +0100

    Updating comments

--- Program.cs
+++ Program.cs
@@ -9,12 +9,18 @@ namespace exampleApp
             Console.WriteLine("Welcome to the Property Management System.");
             Console.WriteLine("The local time is " + DateTime.Now + "\n");
             
+            // Creating Company object.
+
             Company exampleCompany = new Company("Bob Jones", "Aerodynamics Ltd", "Cambridge", "123@gmail.com", "07755555555");
             Console.WriteLine(exampleCompany.ClientProfile() + "\n");
 
+            // Creating Individual object.
+            
             Individual exampleLandlord = new Individual("Marge Simpson", "Springfield", 2, "456@gmail.com", "07766666666");
             Console.WriteLine(exampleLandlord.ClientProfile() + "\n");
 
+            // Creating Service object.
+            
             Service exampleService = new Service("Jane Smith", "London", "Electric Repairs", "Electricity", "789@gmail", "07777777777");
             Console.WriteLine(exampleService.ClientProfile() + "\n");
 
